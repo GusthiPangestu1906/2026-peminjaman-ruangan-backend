45843998c1808d75178c055e2cbd4cd55468eb82
Gusthi Pangestu <gusthipangestu1906@gmail.com>
2026-02-06 19:17:54 +0700

    feat(room): finalize crud operations and enable swagger ui

--- Controllers/RoomController.cs
+++ Controllers/RoomController.cs
@@ -5,7 +5,7 @@ using _2026_peminjaman_ruangan_backend.Models;
 
 namespace _2026_peminjaman_ruangan_backend.Controllers;
 
-[Route("api/room")]
+[Route("api/[controller]")]
 [ApiController]
 public class RoomController : ControllerBase
 {
@@ -21,5 +21,46 @@ public class RoomController : ControllerBase
     public async Task<ActionResult<IEnumerable<Room>>> GetRooms()
     {
         return await _context.Rooms.ToListAsync();
+    }
+
+    // POST: api/room
+    [HttpPost]
+    public async Task<ActionResult<Room>> PostRoom(Room room)
+    {
+        _context.Rooms.Add(room);
+        await _context.SaveChangesAsync();
+        return CreatedAtAction(nameof(GetRooms), new { id = room.Id }, room);
+    }
+
+    // PUT: api/room/5
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutRoom(int id, Room room)
+    {
+        if (id != room.Id) return BadRequest();
+
+        _context.Entry(room).State = EntityState.Modified;
+
+        try {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException) {
+            if (!_context.Rooms.Any(e => e.Id == id)) return NotFound();
+            else throw;
+        }
+
+        return NoContent();
+    }
+
+    // DELETE: api/room/5
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteRoom(int id)
+    {
+        var room = await _context.Rooms.FindAsync(id);
+        if (room == null) return NotFound();
+
+        _context.Rooms.Remove(room);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
     }
 }
